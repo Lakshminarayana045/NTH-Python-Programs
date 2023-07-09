ae8942fd2adf8319e2a8814b16114cc8d782f8e0
LakshmiNarayana  Konanki <131501781+Lakshminarayana045@users.noreply.github.com>
2023-07-09 16:02:16 +0530

    Programs.py

--- Programs.py
+++ Programs.py
@@ -758,7 +758,28 @@ for j in na:
     if len(j) == min(l):
         print(j)
 
-
+#contact book
+'''
+name = []
+phone_numbers = []
+num = int(input("enter no of contact you want to save :"))
+for i in range(num):
+    name = input("name:")
+    phone_number = input("phone number:")
+    names.append(name)
+    phone_numbers.append(phone_number)
+print("\nName\t\t\tPhone Number\n")
+for i in range(num):
+    print("{}\t\t\t{}".format(names[i], phone_numbers[i]))
+search_term = input("\nenter search term:")
+print("search result:")
+if search_term in names:
+    index = names.index(search_term)
+    phone_number = phone_numbers[index]
+    print("name: {}, phone number: {}".format(search_term, phone_number))
+else:
+    print("name not found")
+'''
 '''
 num = eval(input('enter any number: '))
 counter = 0
